41587f71c372ac24bbe6c6d50ff4af9d899ffd99
吴天宇 <86960011+wutianyu1129@users.noreply.github.com>
2023-11-23 15:38:09 +0000

    3

--- test.py
+++ test.py
@@ -1,42 +1,50 @@
-import numpy as np
 from scipy.optimize import minimize
-
-# 定义目标函数
-def objective(x, P, q, r):
-    return 0.5 * np.dot(x, np.dot(P, x)) + np.dot(q, x) + r
-
-# 目标函数的参数
-P = np.array([[13, 12, -2], [12, 17, 6], [-2, 6, 12]])
-q = np.array([-22, -14.5, 13])
-r = 1
-
-# 初始猜测
-x0 = np.array([0, 0, 0])
-
-# 变量的界限
-bounds = [(-1, 1), (-1, 1), (-1, 1)]
-
-# 使用 'SLSQP' 算法进行优化
-result = minimize(objective, x0, args=(P, q, r), method='SLSQP', bounds=bounds)
-
-# 输出结果
-print("Optimal solution:", result.x)
-print("Objective function value at optimal solution:", result.fun)
-
-import time
 import numpy as np
+import matplotlib.pyplot as plt
 
-def benchmark():
-    N = 10000000
-    A = np.random.rand(N)
-    B = np.random.rand(N)
+# Objective function
+def objective(x):
+    return x[0]**2 + 9*x[1]**2
 
-    start_time = time.time()
-    C = A * B  # 一个简单的向量乘法操作
-    end_time = time.time()
+# Constraints
+constraints = [
+    {'type': 'ineq', 'fun': lambda x: 2*x[0] + x[1] - 1},
+    {'type': 'ineq', 'fun': lambda x: x[0] + 3*x[1] - 1},
+    {'type': 'ineq', 'fun': lambda x: x[0]},
+    {'type': 'ineq', 'fun': lambda x: x[1]}
+]
 
-    return end_time - start_time
+# Initial guess
+x0 = [0.5, 0.5]
 
-# 运行跑分
-time_taken = benchmark()
-print(f"Time taken for benchmark: {time_taken:.4f} seconds")
+# Solving the problem
+solution = minimize(objective, x0, constraints=constraints)
+x_opt = solution.x
+
+# Generating data for contours and constraints
+x = np.linspace(0, 1.5, 400)
+y = np.linspace(0, 1.5, 400)
+X, Y = np.meshgrid(x, y)
+Z = X**2 + 9*Y**2
+
+# Constraint lines
+constraint1 = (1 - 2*x)/1
+constraint2 = (1 - x)/3
+
+# Plotting
+plt.figure(figsize=(8, 6))
+plt.contour(X, Y, Z, levels=50, cmap='viridis')
+plt.plot(x, constraint1, label=r'$2x_1 + x_2 \geq 1$')
+plt.plot(x, constraint2, label=r'$x_1 + 3x_2 \geq 1$')
+plt.fill_between(x, np.maximum(0, constraint1), constraint2, where=(constraint2>=constraint1) & (constraint2>=0), color='gray', alpha=0.3)
+plt.scatter(x_opt[0], x_opt[1], color='red', label='Optimal Point')
+plt.xlim([0, 1.5])
+plt.ylim([0, 1.5])
+plt.xlabel('$x_1$')
+plt.ylabel('$x_2$')
+plt.legend()
+plt.title('Feasible Region with Objective Function Contours')
+plt.grid(True)
+plt.show()
+
+x_opt
